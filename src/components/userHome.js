506700c1b4fcab91833618875c2e25b8d3cd2809
Piyush Gupta <guptapiyush315@gmail.com>
2023-05-06 12:25:30 +0530

    abc

---
+++ src/components/userHome.js
@@ -0,0 +1,24 @@
+import React, { Component, useEffect, useState } from "react";
+import ImageUpload from "../components/imageUpload."
+
+export default function UserHome({ userData }) {
+  const logOut = () => {
+    window.localStorage.clear();
+    window.location.href = "./sign-in";
+  };
+  return (
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <div>
+          Name<h1>{userData.fname}</h1>
+          Email <h1>{userData.email}</h1>
+          <br />
+          <button onClick={logOut} className="btn btn-primary">
+            Log Out
+          </button>
+        </div>
+      </div>
+        <ImageUpload/>
+    </div>
+  );
+}
